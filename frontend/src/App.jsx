eca39abb947e85f580ec35a3b39eb88b77b38a50
SonaliGodavarthy <sonali2720@gmail.com>
2026-01-31 14:19:29 +0100

    Add React frontend application with landing page and form page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import LandingPage from './components/LandingPage'
+import FormPage from './components/FormPage'
+import './App.css'
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/form" element={<FormPage />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
+
